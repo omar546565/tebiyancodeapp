0ad4034d5283bed5a777a989a7d6a49899c5a98c
omar546565 <omar546565@gmail.com>
2022-01-16 16:05:52 +0300

    df3355456

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -13,6 +13,7 @@ class LoginScreen extends StatelessWidget {
     LoginScreen ({ Key? key }) : super(key: key);
   var emailController = TextEditingController();
   var passwordController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,109 +23,127 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(10.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children:  [
-              const Text(
-                'login',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                height: 05.0,
-              ),
-              TextFormField(
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                onFieldSubmitted: (String value){
-                // ignore: avoid_print
-                print(value);
-                },
-                onChanged: (String value){
-                // ignore: avoid_print
-                print(value);
-                },
-                decoration: const InputDecoration(
-                  labelText: 'email',
-                  prefixIcon: Icon(
-                    Icons.email,
-                    ),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(
-                height: 05.0,
-                ),
-              TextFormField(
-                controller: passwordController,
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-                onFieldSubmitted: (String value){
-                // ignore: avoid_print
-                print(value);
-                },
-                onChanged: (String value){
-                // ignore: avoid_print
-                print(value);
-                },
-                decoration: const InputDecoration(
-                  labelText: 'password',
-                  prefixIcon: Icon(
-                    Icons.lock,
-                    ),
-                  suffixIcon: Icon(
-                    Icons.remove_red_eye,
-                    ),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(
-                height: 05.0,
-              ),
-               defaultButton(
-                 function: (){
-                     print(emailController.text);
-                     print(passwordController.text);
-                   },
-                 text: 'login',
-                 fontsize: 25.0,
-                   ),
-                const SizedBox(
-                height: 05.0,
-              ),
-               defaultButton(
-                 function: (){
-                     print(emailController.text);
-                     print(passwordController.text);
-                   },
-                 text: 'تسجيل الدخول',
-                 isUpperCase: false,
-                 background: Colors.red,
-                 width: 200.0,
-                 radius: 25.0,
-                   ),
-            const SizedBox(
-                height: 05.0,
-                ),
-              Row( 
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Don\'t have an account?'
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children:  [
+                const Text(
+                  'login',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                  TextButton(
-                    onPressed: (){},
-                    child: 
-                    const Text(
-                      'Register Now',
+                ),
+                const SizedBox(
+                  height: 05.0,
+                ),
+                TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  onFieldSubmitted: (String value){
+                  // ignore: avoid_print
+                  print(value);
+                  },
+                  onChanged: (String value){
+                  // ignore: avoid_print
+                  print(value);
+                  },
+                  validator: (value){
+                    if(value!.isEmpty){
+                      return 'email must not be empty';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'email',
+                    prefixIcon: Icon(
+                      Icons.email,
                       ),
-                  
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 05.0,
+                  ),
+                TextFormField(
+                  controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  onFieldSubmitted: (String value){
+                  // ignore: avoid_print
+                  print(value);
+                  },
+                  onChanged: (String value){
+                  // ignore: avoid_print
+                  print(value);
+                  },
+                  validator: (value){
+                    if(value!.isEmpty){
+                      return 'password must not be empty';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'password',
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      ),
+                    suffixIcon: Icon(
+                      Icons.remove_red_eye,
+                      ),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 05.0,
+                ),
+                 defaultButton(
+                   function: (){
+                     if(formKey.currentState!.validate()){
+                       print(emailController.text);
+                       print(passwordController.text);
+                     }
+
+                     },
+                   text: 'login',
+                   fontsize: 25.0,
+                     ),
+                  const SizedBox(
+                  height: 05.0,
+                ),
+                 defaultButton(
+                   function: (){
+                       print(emailController.text);
+                       print(passwordController.text);
+                     },
+                   text: 'تسجيل الدخول',
+                   isUpperCase: false,
+                   background: Colors.red,
+                   width: 200.0,
+                   radius: 25.0,
+                     ),
+              const SizedBox(
+                  height: 05.0,
+                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Don\'t have an account?'
                     ),
-                ],
-              ),
-            ]),
+                    TextButton(
+                      onPressed: (){},
+                      child:
+                      const Text(
+                        'Register Now',
+                        ),
+
+                      ),
+                  ],
+                ),
+              ]),
+            ),
           ),
         ),
       ),
--- lib/shared/companents/companents.dart
+++ lib/shared/companents/companents.dart
@@ -28,3 +28,4 @@ Widget defaultButton({
     color: background,
   ),
 );
+
