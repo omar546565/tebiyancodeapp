fcc2f2a1f0a95f8ff71c4445e5e29462add50caf
omar546565 <omar546565@gmail.com>
2022-02-07 08:35:11 +0300

    dsf33432

--- lib/modules/shop_app/search-screen/search-screen.dart
+++ lib/modules/shop_app/search-screen/search-screen.dart
@@ -31,6 +31,9 @@ class SearchScreen extends StatelessWidget {
                       type: TextInputType.text,
                       label: 'search',
                       prefix: Icons.print,
+                      onChange:(String text){
+                        ShopSearchCubit.get(context).search(text);
+                      } ,
                       onFieldSubmit: (String text){
                               ShopSearchCubit.get(context).search(text);
                       },
@@ -45,101 +48,104 @@ class SearchScreen extends StatelessWidget {
                     SizedBox(height: 10.0,),
                     if(state is ShopSearchLoadingState)
                     LinearProgressIndicator(),
-                    ShopSearchCubit.get(context).srearchModel!.products!.length != null ? Center(child: Text('لايوجد منتجات في المفضلة',style: TextStyle(fontSize: 24.0,),)) : ListView.separated(
-            itemBuilder: (context,index)=> Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-            height: 120.0,
-            child: Row(
-            children: [
-            Stack(
-            alignment: AlignmentDirectional.bottomStart,
-            children: [
-            Image(
-            image: NetworkImage(ShopSearchCubit.get(context).srearchModel!.products![index].image.toString(),),
-            width: 120.0,
-            height: 200.0,
-            fit: BoxFit.contain,
-            ),
-            if(1 != 0)
-            Container(
-            color: Colors.red,
-            padding: EdgeInsets.symmetric(horizontal: 5.0,) ,
-            child: Text(
-            'Discount',
-            style: TextStyle(
-            fontSize: 12.0,
-            color: Colors.white,
-            ),
-            ),
-            ),
-            ],
-            ),
-            SizedBox(width: 20.0,),
-            Expanded(
-            child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+                    SizedBox(height: 10.0,),
+                    state  is ShopSearchSuccessState ? Expanded(
+                      child: ListView.separated(
+                        itemBuilder: (context,index)=> Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Container(
+                            height: 120.0,
+                            child: Row(
+                              children: [
+                                Stack(
+                                  alignment: AlignmentDirectional.bottomStart,
+                                  children: [
+                                    Image(
+                                      image: NetworkImage(ShopSearchCubit.get(context).srearchModel!.products![index].image.toString(),),
+                                      width: 120.0,
+                                      height: 200.0,
+                                      fit: BoxFit.contain,
+                                    ),
+                                    if(1 != 0)
+                                      Container(
+                                        color: Colors.red,
+                                        padding: EdgeInsets.symmetric(horizontal: 5.0,) ,
+                                        child: Text(
+                                          'Discount',
+                                          style: TextStyle(
+                                            fontSize: 12.0,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                                SizedBox(width: 20.0,),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
 
-            children: [
-            Text(
-              ShopSearchCubit.get(context).srearchModel!.products![index].name.toString(),
-            style: TextStyle(
-            fontSize: 18.0,
-            height: 1.3,
-            fontWeight: FontWeight.bold,
-            ),
-            maxLines: 2,
-            overflow:TextOverflow.ellipsis ,
+                                    children: [
+                                      Text(
+                                        ShopSearchCubit.get(context).srearchModel!.products![index].name.toString(),
+                                        style: TextStyle(
+                                          fontSize: 18.0,
+                                          height: 1.3,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                        maxLines: 2,
+                                        overflow:TextOverflow.ellipsis ,
 
-            ),
-            Spacer(),
-            Row(
-            children:[
-            Text(
-            '${ShopSearchCubit.get(context).srearchModel!.products![index].price.toString()}tl',
-            style: TextStyle(
-            fontSize: 15.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.deepOrange,
-            ),
-            ),
-            SizedBox(width:10.0 ,),
-            if(1 != 0)
-            Text(
-            '${ShopSearchCubit.get(context).srearchModel!.products![index].oldPrice.toString()}tl',
-            style: TextStyle(
-            fontSize: 12.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.grey,
-            decoration: TextDecoration.lineThrough,
-            ),
-            ),
-            Spacer(),
-            IconButton(
-            onPressed: (){
+                                      ),
+                                      Spacer(),
+                                      Row(
+                                          children:[
+                                            Text(
+                                              '${ShopSearchCubit.get(context).srearchModel!.products![index].price}tl',
+                                              style: TextStyle(
+                                                fontSize: 15.0,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.deepOrange,
+                                              ),
+                                            ),
+                                            SizedBox(width:10.0 ,),
+                                            if(1 != 0)
+                                              Text(
+                                                '${ShopSearchCubit.get(context).srearchModel!.products![index].oldPrice}tl',
+                                                style: TextStyle(
+                                                  fontSize: 12.0,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.grey,
+                                                  decoration: TextDecoration.lineThrough,
+                                                ),
+                                              ),
+                                            Spacer(),
+                                            IconButton(
+                                              onPressed: (){
 
-            },
-            icon: CircleAvatar(
-            radius: 15.0,
-            backgroundColor:Colors.deepOrange    ,
-            child: Icon(
-            Icons.favorite_border,
-            color: Colors.white,
-            ),
-            ),
-            ),
-            ]
-            ),
-            ],
-            ),
-            ),
-            ],
-            ),
-            ),
-            ),
-            separatorBuilder: (context,index)=>myDivider(),
-            itemCount: ShopSearchCubit.get(context).srearchModel!.products!.length,
-            )
+                                              },
+                                              icon: CircleAvatar(
+                                                radius: 15.0,
+                                                backgroundColor:Colors.deepOrange    ,
+                                                child: Icon(
+                                                  Icons.favorite_border,
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                            ),
+                                          ]
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        separatorBuilder: (context,index)=>myDivider(),
+                        itemCount: ShopSearchCubit.get(context).srearchModel!.products!.length,
+                      ),
+                    ): Center(child: Text('لايوجد منتجات في المفضلة',style: TextStyle(fontSize: 24.0,),)) ,
                   ],
                 ),
               ),
